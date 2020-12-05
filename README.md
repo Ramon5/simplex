3f4aa89cd05f0c0455733555bc51cd633c238975
Ramon Rodrigues <ramon.srodrigues01@gmail.com>
2020-12-05 18:56:26 -0300

    Added project

---
+++ README.md
@@ -0,0 +1,25 @@
+## Algoritmo Simplex ##
+
+### Requisitos
+
+Linux
+Python >= 3
+
+### Instruções ###
+
+baixe o projeto:
+
+$ git clone 
+
+crie o ambiente virtual e ative:
+
+$ virtualenv -p python3 .env
+$ source .env/bin/activate
+
+Instalando os requisitos:
+
+$ pip install -r requirements.txt
+
+### Executando ###
+
+$ python3 view.py
---
+++ view.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'view.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from simplex import Simplex
+from alert import Ui_Dialog
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(363, 500)
+        MainWindow.setMinimumSize(QtCore.QSize(363, 500))
+        MainWindow.setMaximumSize(QtCore.QSize(363, 500))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("icons/python.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap("icons/python.ico"), QtGui.QIcon.Normal, QtGui.QIcon.On)
+        MainWindow.setWindowIcon(icon)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.campoFO = QtWidgets.QLineEdit(self.centralwidget)
+        self.campoFO.setGeometry(QtCore.QRect(20, 50, 201, 25))
+        self.campoFO.setObjectName("campoFO")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(20, 20, 171, 17))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(20, 90, 67, 17))
+        self.label_2.setObjectName("label_2")
+        self.comboBox = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox.setGeometry(QtCore.QRect(240, 50, 91, 25))
+        self.comboBox.setObjectName("comboBox")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(20, 260, 191, 25))
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap("icons/solve.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton.setIcon(icon1)
+        self.pushButton.setObjectName("pushButton")
+        self.resultPanel = QtWidgets.QTextEdit(self.centralwidget)
+        self.resultPanel.setGeometry(QtCore.QRect(20, 330, 321, 131))
+        self.resultPanel.setReadOnly(True)
+        self.resultPanel.setObjectName("resultPanel")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(20, 300, 141, 17))
+        self.label_3.setObjectName("label_3")
+        self.saBlock = QtWidgets.QTextEdit(self.centralwidget)
+        self.saBlock.setGeometry(QtCore.QRect(20, 110, 311, 131))
+        self.saBlock.setObjectName("saBlock")
+        self.btnClear = QtWidgets.QPushButton(self.centralwidget)
+        self.btnClear.setGeometry(QtCore.QRect(228, 260, 101, 25))
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(QtGui.QPixmap("icons/clear.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.btnClear.setIcon(icon2)
+        self.btnClear.setObjectName("btnClear")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(240, 20, 91, 17))
+        self.label_4.setObjectName("label_4")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.pushButton.clicked.connect(self.execute_simplex)
+        self.btnClear.clicked.connect(self.clear)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Simplex"))
+        self.campoFO.setToolTip(_translate("MainWindow", "Utilize incógnitas diferentes para cada termo. ex: 3x + 3y ao invés de 3x1 + 3x2"))
+        self.label.setText(_translate("MainWindow", "Função Objetivo:"))
+        self.label_2.setText(_translate("MainWindow", "SA:"))
+        self.comboBox.setItemText(0, _translate("MainWindow", "MAX"))
+        self.comboBox.setItemText(1, _translate("MainWindow", "MIN"))
+        self.pushButton.setText(_translate("MainWindow", "Resolver"))
+        self.label_3.setText(_translate("MainWindow", "Resultado:"))
+        self.saBlock.setToolTip(_translate("MainWindow", "Utilize incógnitas diferentes para cada termo de uma restrição"))
+        self.btnClear.setText(_translate("MainWindow", "Limpar"))
+        self.label_4.setText(_translate("MainWindow", "Objetivo:"))
+    
+    def clear(self):
+        self.resultPanel.clear()
+        self.campoFO.setText(None)
+        self.saBlock.setText(None)
+
+    def execute_simplex(self):
+        try:
+            fo = self.campoFO.text()
+            obj = self.comboBox.currentIndex()
+            simplex = Simplex(fo, obj)
+            sa_block = self.saBlock.toPlainText()
+            restrictions = sa_block.split('\n')
+            if len(restrictions) > 0:
+                for sa in restrictions:
+                    simplex.add_constraints(sa)
+                
+                meta = simplex.solve()
+                variables = ""
+                for var in simplex.coefficients:
+                    variables += f"<br/>Valor de {var}: {meta[var]}"
+
+                self.resultPanel.setText(f"<b>Solução Ótima: <span style='color:green;'>{meta['solution']}</span></b>{variables}")
+        
+        except Exception as e:
+            self.show_exception(str(e))
+    
+    def show_exception(self, e: str):
+        Dialog = QtWidgets.QDialog()
+        ui = Ui_Dialog()
+        ui.setupUi(Dialog, e)
+        Dialog.exec()
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
